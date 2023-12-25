85ed3f357357ed4183e2f6c360367a3149f5c22f
NoSpaceAvailable <23520197@gm.uit.edu.vn>
2023-12-25 16:30:26 +0700

    My simple pwnable chall

---
+++ secret.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define isUser 0
+#define isAdmin 1
+
+void adminPanel(){
+    printf("Welcome back, admin!\n");
+    system("/bin/sh");
+}
+
+int checkPassword(int password){
+    return isUser;
+}
+
+int checkCredential(){
+    // char username[] = "John Doe";
+    printf("Welcome, John Doe!\n");
+    printf("Password: ");
+    int Password = isUser;
+    char password[64];
+    scanf("%s", password);
+    return Password;
+}
+
+int main(){
+    if (checkCredential() == isUser){
+        printf("Login successfully.\n" 
+                "Here is your secret: " 
+                "NB2HI4DTHIXS6YTJORWHSLTXOMXTGNZXN5GA====\n");
+    }
+    else{
+        adminPanel();
+    }
+    return 0;
+}
